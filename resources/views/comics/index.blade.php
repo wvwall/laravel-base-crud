fd4465dd071f923041a7743a58951da3d58cf2ff
wvwall <waltervelardo@gmail.com>
2021-05-20 15:25:10 +0200

    Completed prima parte

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,8 +3,8 @@
 @section('main')
     @foreach($comics as $comic)
         <div class="card">
-            <a href="{{route('comics.show', ['comic => $comic->id])}}">
-                <img src="{{comic->thumb}}" alt="{{$comic->title}}">
+            <a href="{{route('comics.show', ['comic' => $comic->id])}}">
+                <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
                 <h3>{{$comic->title}}</h3>
                 <h4><>{{$comic->type}} - {{$comic->price}}$ - {{$comic->series}}</h4>
                 <h5>{{$comic->sale_date}}</h5>
